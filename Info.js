c8843b9a1d924712961056fe6eb629c73635bea2
GooseoftheGeese <anorambuena28@students.stab.org>
2025-01-16 10:04:17 -0500

    Update Info.js

--- Info.js
+++ Info.js
@@ -1,13 +1,17 @@
 var url="https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Culture%20%26%20Entertainment/Netflix%20Content.csv"
 
-var types= getColumn(url,1);
-var titles= getColumn(url,2); 
-var countries= getColumn(url,3);
+var type= getColumn(url,1);
+var title= getColumn(url,2); 
+var country= getColumn(url,3);
 var addedToNetflix= getColumn(url,4);
-var releaseDates= getColumn(url,5);
-var ratings= getColumn(url,6);
-var genres= getColumn(url,7);
+var releaseDate= getColumn(url,5);
+var rating= getColumn(url,6);
+var genre= getColumn(url,7);
 
+
+// returns the information of a certain show
+// contentName{string}chosen show/movie
+// return{list} all information pertaining to the specified show/movie
 function getInformationFromTitle(contentName){
     var info=[]
     for(var i = 0; i<title.length; i++){
@@ -16,4 +20,4 @@ function getInformationFromTitle(contentName){
             return info}
     }return -1
   
-} console.log(getInformationFromTitle("Only the Dead"))         
+} console.log(getInformationFromTitle())         
