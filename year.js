78c18957917395432fcc9507f8800297a360956e
DRussCSP <drussell28@students.stab.org>
2025-01-16 10:01:42 -0500

    Add files via upload

--- year.js
+++ year.js
@@ -1,7 +1,7 @@
 var url="https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Culture%20%26%20Entertainment/Netflix%20Content.csv";
 
 var types=getColumn(url,1);
-var titles=getColumn(url,2);
+var movies=getColumn(url,2);
 var countries=getColumn(url,3);
 var releaseYears=getColumn(url,5);
 var ratings=getColumn(url,6);
